5a09b2f418a33b857074ec5a1dd618701863a96a
AyatT12 <105029664+AyatT12@users.noreply.github.com>
2025-09-03 11:57:29 +0300

    Update page.js

--- app/Posts/[postId]/page.js
+++ app/Posts/[postId]/page.js
@@ -1,11 +1,13 @@
 import PostDestails from "@/app/components/PostDestails";
 import { Suspense } from "react";
 export default async function postDetailsPage({ params }) {
-
+const LoadingJsx=(
+    <div>Loading...</div>
+);
   return (
     <div>
       <h1>Post Details Page</h1>
-     <Suspense>
+     <Suspense fallback={LoadingJsx}>
       <PostDestails postId={params.postId}/>
      </Suspense>
     </div>
